19d085db828b463f63eb824200b01b54ebd60744
unknown <gft07@GFTJ_7.SINENSIA.EDU>
2017-03-22 13:41:48 +0100

    Docker new classes

---
+++ src/test/java/com/eviltester/webdriver/DockerRegistrationTest.java
@@ -0,0 +1,7 @@
+package com.eviltester.webdriver;
+
+/**
+ * Created by gft07 on 3/22/2017.
+ */
+public class DockerRegistrationTest {
+}
---
+++ src/test/java/com/eviltester/webdriver/pageobject/FirstPageObject.java
@@ -0,0 +1,7 @@
+package com.eviltester.webdriver.pageobject;
+
+/**
+ * Created by gft07 on 3/22/2017.
+ */
+public class FirstPageObject {
+}
